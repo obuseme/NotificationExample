057f9d06edbb75cad865e3aefee87ac0ae885ad1
Andy Obusek <andyo@aweber.com>
2016-06-21 21:36:29 -0400

    Initial Commit

---
+++ NotificationExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotificationExample
+//
+//  Created by Andy Obusek on 6/21/16.
+//  Copyright © 2016 Clean Swifter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
